3fc825e1f89ecf3749b01760df04b18088162fd1
Jess Hottenstein <jess.hottenstein@gmail.com>
2014-08-11 17:22:14 -0400

    Made non_empty work with all encodings
    * Memoized non_empty regexp for speed
    * Had to skip ISO-2022-JP-2 and UTF-7 because there was no way to create
      the string (iconv is deprecated from 1.9 and removed from 2.0)

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,21 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str.encode!("US-ASCII") if str.encoding.dummy?
+    str =~ NON_EMPTY_REGEXPS[encoding_name(str)]
+  end
+
+private
+
+  NON_EMPTY_REGEXPS = Hash.new do |h, key|
+    h[key] = Regexp.new('^.+$'.encode(key))
+  end
+  private_constant :NON_EMPTY_REGEXPS
+
+  def self.encoding_name(str)
+    if str.encoding.ascii_compatible? 
+      return "US-ASCII"
+    else
+      return str.encoding.name
+    end
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      next if ["ISO-2022-JP-2", "UTF-7"].include?(enc.name)
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
